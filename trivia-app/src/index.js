e355c253e5696f0d876b96d727016c8b27d52c56
Rae Abunahla <raghebabn@gmail.com>
2021-04-30 13:02:31 -0700

    Multiple cross-over stories complete: Users are now able to answer questions, increment their total score by answering correctly and are informed if they're wrong or not. Users will not be able to answer/modify questions already answered.

--- trivia-app/src/index.js
+++ trivia-app/src/index.js
@@ -6,39 +6,49 @@ import reportWebVitals from './reportWebVitals';
 import questions from './questions.json';
 
 
-// Make Game, Board, Card & Question functions/component classes.
-
 /**
- * Game Component:
- * - Randomly selects 16 questions out of the 40.
+ * The Game component stores the game's state and updates the score for the user. It also assesses whether the answers to the questions the user selected are correct or incorrect.
  */
-
 class Game extends React.Component {
 
   constructor(props) {
     super(props);
-    var questionsSet = this.generateRandomQuestions();
     this.state = {
-      userScore: 0
+      userScore: 0,
+      testQuestions: this.generateRandomQuestions(),
+      answeredQuestions: {}
     };
-
   }
 
-  pointGained(point) {
-    this.setState({
-      userScore: this.state.userScore+1
-    });
+  /**
+   * Updates the state variable with the updated user score.
+   */
+  questionAnswered(questionFull, optionSelected) {
+    if (optionSelected.option === questionFull.correct) {
+      this.state.answeredQuestions[questionFull.id]  = true;
+      this.setState({
+        userScore: this.state.userScore+1,
+        answeredQuestions: this.state.answeredQuestions
+      });
+    } else {
+      this.state.answeredQuestions[questionFull.id] = false;
+      this.setState({
+        answeredQuestions: this.state.answeredQuestions
+      });
+    }
   }
 
-
-
-  // We need a handle click here to take in the question that was being answered. It has a to increment or keep the userScore the same and alter the score appearing on the as well as nullify the buttons.
-
+  
+  /**
+   * Selects 16 out of 40 questions randomly to be used for the Trivia challenge.
+   * @returns the ID's of the questions from the JSON file.
+   */
   generateRandomQuestions() {
     var questionTracker = []
     let i = 0;
     while (i < 16) {
       var random = questions[Math.floor(Math.random() * questions.length)];
+      random.answered = null;
       if (!questionTracker.includes(random.id)) {
         i++; 
         questionTracker.push(random.id);
@@ -52,23 +62,29 @@ class Game extends React.Component {
       <div className="game">
         <h1>Rae's Trivia Challenge</h1> 
         <h2> Your score: {this.state.userScore}/16</h2>
-          <Board questionSet={this.generateRandomQuestions()} 
-          onClick={() => this.pointGained()}/>
+          <Board questionSet={this.state.testQuestions} answeredQuestions={this.state.answeredQuestions}
+          onClick={(questionFull, optionSelected) => this.questionAnswered(questionFull, optionSelected)}/>
       </div>
     )
   }
 }
 
-// Board component:
+/**
+ * The Board component lives within the Game component and also has 16 Card children mapped from the questions randomly selected in the state. It iterates through the IDs and renders thems.
+ */
 class Board extends React.Component {
 
+  /**
+   * Renders all 16 Card components.
+   * @returns the HTML elements.
+   */
   renderCards() {
     const questionsTracker = this.props.questionSet;
     return (
       <div className="cards"> 
         {questions.map((question, key) => {
               if(questionsTracker.includes(question.id))
-                 return(<Card questionInfo={question} category={question.category} onClick={() => this.props.onClick()}></Card>)
+                 return(<Card questionInfo={question} category={question.category} onClick={(questionFull, optionSelected) => this.props.onClick(questionFull, optionSelected)} answeredQuestions={this.props.answeredQuestions}></Card>)
 
           } 
         )}
@@ -90,12 +106,13 @@ class Board extends React.Component {
 
 /**
  * This component class structures the cards such that they contain from the JSON file the category, the options, and the question itself. Upon submission, the user will be informed for the score.
- * 
- * TO UPDATE: Perhaps put the iteration in
  */
 class Card extends React.Component {
 
-
+  /**
+   * Colours the card based on the category passed through the props.
+   * @returns the colour of the card in String format.
+   */
   findColour(category) {
     var colour = "white";
     if (category === "Sports") {
@@ -110,25 +127,49 @@ class Card extends React.Component {
     return colour;
   }
 
+  // I wonder if there's a cleaner way to do this?
   render() {
-
-    var score = "?";
-
-    return (
-      <div className="card" style={{background: this.findColour(this.props.category)}}>
-        <p> Category: {this.props.questionInfo.category}</p>
-        <p> Score: {score}/1</p>
-        <p key={this.props.questionInfo.id}> {this.props.questionInfo.question} </p>
-          {this.props.questionInfo.options.map(option => 
-            <button onClick={() => this.scoreUser(option, this.props.questionInfo.correct)} onClick={() => this.props.onClick()}>{option}</button> 
-          )}
-      </div>
-    )} 
+    const answeredQuestions = this.props.answeredQuestions;
+    const questionInfo = this.props.questionInfo;
+    var score = "?"
+    console.log(answeredQuestions);
+    if (answeredQuestions[questionInfo.id] != null) {
+      score = answeredQuestions[questionInfo.id] ? "1" : "0";
+      return (
+        <div className="card" style={{background: this.findColour(this.props.category)}}>
+          <p> Category: {this.props.questionInfo.category}</p>
+          <p> Score: {score}/1</p>
+          <p key={this.props.questionInfo.id}> {this.props.questionInfo.question} </p>
+            {
+              this.props.questionInfo.options.map(option =>  
+              <button disabled="true" onClick={() => this.props.onClick(this.props.questionInfo, {option})} category={this.props.questionInfo.category}>{option}</button>  
+            )}
+            {
+             answeredQuestions[questionInfo.id] && 
+             <p style={{color: "green", fontWeight: "bold"}}> Right! </p> 
+            }
+            {!answeredQuestions[questionInfo.id] && 
+             <p style={{color: "red", fontWeight: "bold"}}> Wrong... :( </p>
+            }
+            
+        </div>
+      )} else {
+        return (
+          <div className="card" style={{background: this.findColour(this.props.category)}}>
+            <p> Category: {this.props.questionInfo.category}</p>
+            <p> Score: {score}/1</p>
+            <p key={this.props.questionInfo.id}> {this.props.questionInfo.question} </p>
+              {
+                this.props.questionInfo.options.map(option => 
+                <button onClick={() => this.props.onClick(this.props.questionInfo, {option})} category={this.props.questionInfo.category} >{option}</button> 
+              )}
+          </div>
+        )}
+    }
 }
 
 
 // ========================
-
 ReactDOM.render(
   <React.StrictMode>
     <Game/>
