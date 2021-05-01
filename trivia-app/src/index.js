be15f0112e065ed897cac0090723159634f3e8d0
Rae Abunahla <raghebabn@gmail.com>
2021-04-30 17:12:27 -0700

    Some stying and comment clean up

--- trivia-app/src/index.js
+++ trivia-app/src/index.js
@@ -16,12 +16,14 @@ class Game extends React.Component {
     this.state = {
       userScore: 0,
       testQuestions: this.generateRandomQuestions(),
-      answeredQuestions: {}
+      answeredQuestions: {},
+      gameFinished: false
     };
+    this.resetGame = this.resetGame.bind(this);
   }
 
   /**
-   * Updates the state variable with the updated user score.
+   * Updates the state variable with the updated user score. If the game is finished (as in, all the questions are answered) then a window alert informs the users with a message and a reset game button appears.
    */
   questionAnswered(questionFull, optionSelected) {
     if (optionSelected.option === questionFull.correct) {
@@ -31,13 +33,38 @@ class Game extends React.Component {
         answeredQuestions: this.state.answeredQuestions
       });
     } else {
+
       this.state.answeredQuestions[questionFull.id] = false;
       this.setState({
+        userScore: this.state.userScore-1,
         answeredQuestions: this.state.answeredQuestions
       });
     }
+
+    var mapSize = this.getAnsweredCount(this.state.answeredQuestions); // This piece of the checks if all the questions have been answered.
+    if (mapSize == 16) {
+      this.setState({
+        gameFinished: true
+      });
+      if (this.state.userScore < 16) {
+        window.alert("Game over! Better luck next time ;)!");
+      } else {
+        window.alert("Congratulations! You've got them all correct. Have another go :D!");
+      }
+    }
   }
 
+  /**
+   * Gets the total number of questions answered based on the map of questioned provided.
+   * @returns size as an integer of the map.
+   */
+  getAnsweredCount(answeredQuestions) {
+    var count = 0;
+    for (var item in answeredQuestions) {
+      count++;
+    }
+    return count;
+  }
   
   /**
    * Selects 16 out of 40 questions randomly to be used for the Trivia challenge.
@@ -57,11 +84,39 @@ class Game extends React.Component {
     return questionTracker;
   }
 
+  /**
+   * Returns the colour that the score HTML tag should be based on it's value.
+   */
+  findScoreColour() {
+    if (this.state.userScore == 0) {
+      return "black";
+    } else if (this.state.userScore < 0) {
+      return "red";
+    } else {
+      return "green";
+    }
+  }
+
+  /**
+   * Reset game with a new(er) set of questions and cleans the state.
+   */
+  resetGame() {
+    this.setState({
+      userScore: 0,
+      testQuestions: this.generateRandomQuestions(),
+      answeredQuestions: {},
+      gameFinished: false
+    });
+  }
+
   render() {
     return(
       <div className="game">
-        <h1>Rae's Trivia Challenge</h1> 
-        <h2> Your score: {this.state.userScore}/16</h2>
+        <h1 class="title">Rae's Trivia Challenge</h1> 
+        <h2 class="sub-title" style={{color: this.findScoreColour()}}> Your score: {this.state.userScore}/16</h2>
+          {this.state.gameFinished &&
+            <button onClick={this.resetGame}> Reset Game</button>
+          }
           <Board questionSet={this.state.testQuestions} answeredQuestions={this.state.answeredQuestions}
           onClick={(questionFull, optionSelected) => this.questionAnswered(questionFull, optionSelected)}/>
       </div>
@@ -92,12 +147,11 @@ class Board extends React.Component {
     );
   }
 
-  // This needs to loop through all the questions and display them onto the board.
-  render() {
+  render() { // The render card function does the heavy-lifting for card generation.
     return (
       <div>
-        <div className="board-row">
-          {this.renderCards()}
+        <div className="board-row" class="cards">
+          {this.renderCards()} 
         </div>
       </div>
     );
@@ -127,16 +181,19 @@ class Card extends React.Component {
     return colour;
   }
 
-  // I wonder if there's a cleaner way to do this?
+  /**
+   * ((Disclaimer: I'm guessing there's a cleaner way to do this.))
+   * 
+   * The render function here checks if the question has been answered or not before populating the buttons are disabled or not (as well as updated the score of that particular question).
+   */
   render() {
     const answeredQuestions = this.props.answeredQuestions;
     const questionInfo = this.props.questionInfo;
     var score = "?"
-    console.log(answeredQuestions);
     if (answeredQuestions[questionInfo.id] != null) {
       score = answeredQuestions[questionInfo.id] ? "1" : "0";
       return (
-        <div className="card" style={{background: this.findColour(this.props.category)}}>
+        <div className="card" class="card" style={{background: this.findColour(this.props.category)}}>
           <p> Category: {this.props.questionInfo.category}</p>
           <p> Score: {score}/1</p>
           <p key={this.props.questionInfo.id}> {this.props.questionInfo.question} </p>
@@ -151,7 +208,6 @@ class Card extends React.Component {
             {!answeredQuestions[questionInfo.id] && 
              <p style={{color: "red", fontWeight: "bold"}}> Wrong... :( </p>
             }
-            
         </div>
       )} else {
         return (
@@ -161,7 +217,7 @@ class Card extends React.Component {
             <p key={this.props.questionInfo.id}> {this.props.questionInfo.question} </p>
               {
                 this.props.questionInfo.options.map(option => 
-                <button onClick={() => this.props.onClick(this.props.questionInfo, {option})} category={this.props.questionInfo.category} >{option}</button> 
+                <button variant="secondary" onClick={() => this.props.onClick(this.props.questionInfo, {option})} category={this.props.questionInfo.category} >{option}</button> 
               )}
           </div>
         )}
